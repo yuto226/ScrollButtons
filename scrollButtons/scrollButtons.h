c2e6329dac61acc38f018aa4f15810eebd38da97
Yuto Suzuki <yuto-suzuki@navitime.co.jp>
2022-01-04 14:00:17 +0900

    Initial Commit

---
+++ scrollButtons/scrollButtons.h
@@ -0,0 +1,18 @@
+//
+//  scrollButtons.h
+//  scrollButtons
+//
+//  Created by suzuki-yuto on 2022/01/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for scrollButtons.
+FOUNDATION_EXPORT double scrollButtonsVersionNumber;
+
+//! Project version string for scrollButtons.
+FOUNDATION_EXPORT const unsigned char scrollButtonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <scrollButtons/PublicHeader.h>
+
+
